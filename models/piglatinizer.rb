940b645f815339bfa170c90ac7584fe35adf6c9a
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-29 17:57:48 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,10 +3,8 @@ class PigLatinizer
   def piglatinize(word)
     key = word.index("#{word.scan(/[aeiouAEIOU]/).first}")
 
-    #checks to see if second letter is a vowel
       pigword = word.slice!(0,key)
       word << "#{pigword}#{"w" if key == 0}ay"
-
   end
 
 end
