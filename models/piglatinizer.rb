58ccee821a87c32e20a458dadd28445566797330
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-29 16:55:44 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,12 +1,12 @@
 class PigLatinizer
   attr_accessor :words
 
-  def initialize(:words)
+  def initialize(word)
     @words = word
   end
 
-  def piglatinize
-    pigword = @words.slice!(0)
-    @words << "#{pigword}ay"
-  end
+  #def piglatinize
+  #  pigword = @words.slice!(0)
+  #  @words << "#{pigword}ay"
+  #end
 end
