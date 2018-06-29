618244ae381fba73c9ae56855ee3e7a4bf71cf8f
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-29 16:52:22 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,12 @@
 class PigLatinizer
   attr_accessor :word
-  
+
   def initialize(word)
     @word = word
+  end
+
+  def piglatinize
+    pigword = @word.slice!(0)
+    @word << pigword
   end
 end
