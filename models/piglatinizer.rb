74afed1b28ea63f7f38a9d7f34bd105e478913c6
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-29 17:07:29 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,7 @@
 class PigLatinizer
 
   def piglatinize(word)
-    if word[1].include?(/[aeiou]/)
+    if word[1].include?(/[aeoui]/)
       pigword = word.slice!(0)
       word << "#{pigword}ay"
     end
