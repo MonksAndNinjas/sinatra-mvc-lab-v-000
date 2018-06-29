d3f21f1443f0bf38cefdb26a589fe9593c773bff
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-29 17:26:52 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,7 +2,9 @@ class PigLatinizer
 
   def piglatinize(word)
     #checks to see if second letter is a vowel
-    if word[0] == ["a","e","i","o","u"]
+    if word[0] == ["a","e","i","o","u"] and word.size == 1
+      word << "way"
+
       pigword = word.slice!(0)
       word << "#{pigword}ay"
   end
