e763303713721fac51481a8b0791bd23d3d1fe28
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-29 18:04:46 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,11 +3,14 @@ class PigLatinizer
   def piglatinize(sentence)
     #splits sentence into an array of words
     words = sentence.split(" ")
-    #finds the first vowel to set at the slicing point
-    key = words.index("#{word.scan(/[aeiouAEIOU]/).first}")
-    #slices and then appends the consonants before the first vowel
-    pigword = word.slice!(0,key)
-    word << "#{pigword}#{"w" if key == 0}ay"
+
+    words.each do |word|
+      #finds the first vowel to set at the slicing point
+      key = words.index("#{word.scan(/[aeiouAEIOU]/).first}")
+      #slices and then appends the consonants before the first vowel
+      pigword = word.slice!(0,key)
+      word << "#{pigword}#{"w" if key == 0}ay"
+    end
   end
 
 
