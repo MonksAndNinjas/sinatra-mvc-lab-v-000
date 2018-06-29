10e2654780a94722248cf5a082cec0231c727319
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-29 17:17:55 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,14 @@
 class PigLatinizer
 
   def piglatinize(word)
+    #checks to see if second letter is a vowel
     if !word[1].scan(/[aeoui]/).empty?
       pigword = word.slice!(0)
       word << "#{pigword}ay"
-    elsif word[1].scan(/[bcdfghjklmnpqrstvwxyz]/) 
+    #checks to see if second letter is a consonant
+    elsif !word[1].scan(/[bcdfghjklmnpqrstvwxyz]/).empty?
+
+    end
   end
 
 end
