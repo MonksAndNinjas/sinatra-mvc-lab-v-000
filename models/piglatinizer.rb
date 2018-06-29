004edd60ee60e03fa893bfd00e21ea38504e702f
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-29 17:05:46 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,9 +1,7 @@
 class PigLatinizer
 
   def piglatinize(word)
-    case word
-
-    when word[1] == /[aeiou]/
+    if word[1] == /[aeiou]/
     pigword = word.slice!(0)
     word << "#{pigword}ay"
     end
