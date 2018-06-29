dcdf59e28cadc5daa881c8ff415845fb9bb797b2
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-29 17:55:47 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,8 +4,14 @@ class PigLatinizer
   key = word.index("#{word.scan(/[aeiouAEIOU]/).first}")
 
     #checks to see if second letter is a vowel
-      pigword = word.slice!(0,key)
+    if key == 0 
       word << "#{pigword}way"
+
+    else
+      pigword = word.slice!(0,key)
+      word << "#{pigword}ay"
+    end
+
   end
 
 end
