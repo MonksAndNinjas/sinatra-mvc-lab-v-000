4bd3061f417ef01896418822b9ce64e1ef31feb4
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-29 17:20:41 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,11 +2,11 @@ class PigLatinizer
 
   def piglatinize(word)
     #checks to see if second letter is a vowel
-    if !word[1].scan(/[aeoui]/).empty?
+    if !word[1].scan(/[aeoui]/).empty? and word[0].scan(/[aeiou]/).empty?
       pigword = word.slice!(0)
       word << "#{pigword}ay"
     #checks to see if second letter is a consonant
-    elsif !word[1].scan(/[bcdfghjklmnpqrstvwxyz]/).empty?
+    elsif word[0].scan(/[bcdfghjklmnpqrstvwxyz]/).empty?
 
     end
   end
